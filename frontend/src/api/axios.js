683f997adafdcfaccdb2466e893f90d46487baf3
Shelema Ageri <shelemaagari@gmail.com>
2026-08-12 12:06:20 +0400

    Update production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://tech-electronics-backend.onrender.com/api"
 });
 
 // Send JWT token automatically
